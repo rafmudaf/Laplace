4c819a48fda941dc6dcfed1227794c0e855ae562
Rafael M Mudafort <rafmudaf@gmail.com>
2017-01-02 00:30:19 -0600

    add glkit camera capturing and rendering

--- openShutterVideo/ViewController.swift
+++ openShutterVideo/ViewController.swift
@@ -8,166 +8,64 @@
 
 import UIKit
 import AVFoundation
+import GLKit
+//import CoreImage
+//import OpenGLES
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CameraControllerDelegate {// AVCaptureVideoDataOutputSampleBufferDelegate,  {
+
+    @IBOutlet weak var videoPreviewView: UIView!
     
-    let captureSession = AVCaptureSession()
-    var captureDevice: AVCaptureDevice?
-    var previewLayer = AVCaptureVideoPreviewLayer()
-    let stillImageOutput = AVCaptureStillImageOutput()
-    
-    var beginImage: CIImage!
-    
-    @IBOutlet weak var imageView: UIImageView!
-    
+    var glContext: EAGLContext?
+    var ciContext: CIContext?
+    var renderBuffer: GLuint = GLuint()
+    var glView: GLKView?
+
+    var cameraController: CameraController!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        captureSession.sessionPreset = AVCaptureSessionPresetHigh
         
-        let devices = AVCaptureDevice.devices()
+        cameraController = CameraController(previewType: .Manual, delegate: self)
         
-        for device in devices {
-            if (device.hasMediaType(AVMediaTypeVideo)) {
-                if (device.position == AVCaptureDevicePosition.Back) {
-                    captureDevice = device as? AVCaptureDevice
-                }
-            }
+        glContext = EAGLContext(API: .OpenGLES2)
+        
+        glView = GLKView(frame: videoPreviewView.frame, context: glContext!)
+        glView!.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_2))
+        if let window = glView!.window {
+            glView!.frame = window.bounds
         }
         
-        if captureDevice != nil {
-            beginSession()
+        ciContext = CIContext(EAGLContext: glContext!)
+        
+        videoPreviewView.addSubview(glView!)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraController.startRunning()
+    }
+    
+//    func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
+//        let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+//        let image = CIImage(CVPixelBuffer: pixelBuffer!)
+//        if glContext != EAGLContext.currentContext() {
+//            EAGLContext.setCurrentContext(glContext)
+//        }
+//        glView!.bindDrawable()
+//        ciContext!.drawImage(image, inRect: image.extent, fromRect: image.extent)
+//        glView!.display()
+//    }
+    
+    // MARK: - CameraControllerDelegate
+    func cameraController(cameraController: CameraController, didDetectFaces faces:Array<(id:Int,frame:CGRect)>) { }
+    
+    func cameraController(cameraController: CameraController, didOutputImage image: CIImage) {
+        if glContext != EAGLContext.currentContext() {
+            EAGLContext.setCurrentContext(glContext)
         }
-    }
-    
-    func beginSession() {
-        do {
-            configureDevice()
-            try self.captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
-            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            self.view.layer.addSublayer(previewLayer)
-            previewLayer.frame = self.view.layer.frame
-            captureSession.startRunning()
-        } catch let error as NSError {
-            print("error: \(error.localizedDescription)")
-        }
-    }
-    
-    func configureDevice() {
-        if let device = captureDevice {
-            do {
-                try device.lockForConfiguration()
-                device.focusMode = .Locked
-                device.unlockForConfiguration()
-            } catch let error as NSError {
-                print("error: \(error.localizedDescription)")
-            }
-        }
-    }
-    
-    func updateDeviceSettings(focusValue: Float, isoValue: Float) {
-        if let device = captureDevice {
-            do {
-                try device.lockForConfiguration()
-                device.setFocusModeLockedWithLensPosition(focusValue, completionHandler: { (time) -> Void in
-                    //
-                })
-                
-                let minISO = device.activeFormat.minISO
-                let maxISO = device.activeFormat.maxISO
-                let clampedISO = isoValue * (maxISO - minISO) + minISO
-                
-                device.setExposureModeCustomWithDuration(AVCaptureExposureDurationCurrent, ISO: clampedISO, completionHandler: { (time) -> Void in
-                    //
-                })
-                
-                device.unlockForConfiguration()
-            } catch let error as NSError {
-                print("error: \(error.localizedDescription)")
-            }
-        }
-    }
-    
-    func updateLayer(opacityValue: Float) {
-        previewLayer.opacity = opacityValue
-    }
-    
-    func touchPercent(touch: UITouch) -> CGPoint {
-        let screenSize = UIScreen.mainScreen().bounds.size
-        var touchPer = CGPointZero
-        touchPer.x = touch.locationInView(self.view).x / screenSize.width
-        touchPer.y = touch.locationInView(self.view).y / screenSize.height
-        return touchPer
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        let touchPer = touchPercent(touches.first! as UITouch)
-        updateDeviceSettings(Float(touchPer.x), isoValue: Float(touchPer.y))
-        updateLayer(Float(touchPer.x))
-    }
-    
-    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        let touchPer = touchPercent(touches.first! as UITouch)
-        updateDeviceSettings(Float(touchPer.x), isoValue: Float(touchPer.y))
-        updateLayer(Float(touchPer.x))
-    }
-    
-    func saveToCamera(sender: UITapGestureRecognizer) {
-        if let videoConnection = stillImageOutput.connectionWithMediaType(AVMediaTypeVideo) {
-            stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection) {
-                (imageDataSampleBuffer, error) -> Void in
-                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
-                UIImageWriteToSavedPhotosAlbum(UIImage(data: imageData)!, nil, nil, nil)
-            }
-        }
+        glView!.bindDrawable()
+        ciContext?.drawImage(image, inRect: image.extent, fromRect: image.extent)
+        glView!.display()
     }
 }
-
-
-
-
-
-
-
-
-//import UIKit
-//import AVFoundation
-//class ViewController: UIViewController {
-//    let captureSession = AVCaptureSession()
-//    let stillImageOutput = AVCaptureStillImageOutput()
-//    var error: NSError?
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        let devices = AVCaptureDevice.devices().filter{ $0.hasMediaType(AVMediaTypeVideo) && $0.position == AVCaptureDevicePosition.Back }
-//        if let captureDevice = devices.first as? AVCaptureDevice  {
-//            
-//            captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &error))
-//            captureSession.sessionPreset = AVCaptureSessionPresetPhoto
-//            captureSession.startRunning()
-//            stillImageOutput.outputSettings = [AVVideoCodecKey:AVVideoCodecJPEG]
-//            if captureSession.canAddOutput(stillImageOutput) {
-//                captureSession.addOutput(stillImageOutput)
-//            }
-//            if let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) {
-//                previewLayer.bounds = view.bounds
-//                previewLayer.position = CGPointMake(view.bounds.midX, view.bounds.midY)
-//                previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
-//                let cameraPreview = UIView(frame: CGRectMake(0.0, 0.0, view.bounds.size.width, view.bounds.size.height))
-//                cameraPreview.layer.addSublayer(previewLayer)
-//                cameraPreview.addGestureRecognizer(UITapGestureRecognizer(target: self, action:"saveToCamera:"))
-//                view.addSubview(cameraPreview)
-//            }
-//        }
-//    }
-//    func saveToCamera(sender: UITapGestureRecognizer) {
-//        if let videoConnection = stillImageOutput.connectionWithMediaType(AVMediaTypeVideo) {
-//            stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection) {
-//                (imageDataSampleBuffer, error) -> Void in
-//                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
-//                UIImageWriteToSavedPhotosAlbum(UIImage(data: imageData), nil, nil, nil)
-//            }
-//        }
-//    }
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//    }
-//}
