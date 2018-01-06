370fbe11fdfbdad269a01d206b0c46e7bcb63f4d
Rafael M Mudafort <rafmudaf@gmail.com>
2018-01-06 16:00:19 -0700

    remove unused files and rename bridging header

---
+++ LongExposureVideo/bridgingheader.h
@@ -0,0 +1,14 @@
+//
+//  bridgingheader.h
+//  LongExposureVideo
+//
+//  Created by Rafael M Mudafort on 12/31/16.
+//  Copyright © 2016 Rafael M Mudafort. All rights reserved.
+//
+
+#ifndef bridgingheader_h
+#define bridgingheader_h
+
+#import "CVWrapper.h"
+
+#endif /* bridgingheader_h */
