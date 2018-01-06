e92570c25ac575da7812a6a79491fded3713838b
Rafael M Mudafort <rafmudaf@gmail.com>
2018-01-06 15:59:09 -0700

    project updates

--- LongExposureVideo/AppDelegate.swift
+++ LongExposureVideo/AppDelegate.swift
@@ -40,7 +40,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     @nonobjc func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
